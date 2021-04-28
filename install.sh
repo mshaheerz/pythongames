04e709551fea989dec0e40835909f4b7bf0cf19f
shaheer <72137242+mshaheerz@users.noreply.github.com>
2021-04-28 15:06:28 +0530

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,13 @@
+apt-get install python2
+apt-get install python
+pip install requests
+pip install turtle
+pip install freegames
+pip install shuffle
+pip install square
+pip install line 
+pip install path
+pip install randrange
+pip install seed
+pip install floor
+#shrz
